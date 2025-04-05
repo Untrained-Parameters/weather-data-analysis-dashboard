accc3c7b6b9a759eaa55bdc950849e4fc2b03268
dhvanil <dddesai@hawaii.edu>
2025-04-04 19:01:36 -1000

    ...

--- weather_dashboard_webpage.py
+++ weather_dashboard_webpage.py
@@ -5,7 +5,6 @@ import streamlit as st
 import altair as alt
 import matplotlib.pyplot as plt 
 
-
 # this is a test
 # setting page configuration
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
