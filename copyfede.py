b8ceadc093bbb330e49dcc8ef0869abaf3f4b20a
Fede <fchiti@hawaii.edu>
2025-04-05 23:12:09 -1000

    plots

--- copyfede.py
+++ copyfede.py
@@ -232,51 +232,51 @@ main_col, chat_col = st.columns([4,1])
 
 
 
-# def get_chart_98185(use_container_width: bool):
+def get_chart_98185(use_container_width: bool):
 
-#     source = data.seattle_weather.url
-#     step = 20
-#     overlap = 1
+    source = data.seattle_weather.url
+    step = 20
+    overlap = 1
 
-#     chart = alt.Chart(source, height=step).transform_timeunit(
-#         Month='month(date)'
-#     ).transform_joinaggregate(
-#         mean_temp='mean(temp_max)', groupby=['Month']
-#     ).transform_bin(
-#         ['bin_max', 'bin_min'], 'temp_max'
-#     ).transform_aggregate(
-#         value='count()', groupby=['Month', 'mean_temp', 'bin_min', 'bin_max']
-#     ).transform_impute(
-#         impute='value', groupby=['Month', 'mean_temp'], key='bin_min', value=0
-#     ).mark_area(
-#         interpolate='monotone',
-#         fillOpacity=0.8,
-#         stroke='lightgray',
-#         strokeWidth=0.5
-#     ).encode(
-#         alt.X('bin_min:Q', bin='binned', title='Maximum Daily Temperature (C)'),
-#         alt.Y(
-#             'value:Q',
-#             scale=alt.Scale(range=[step, -step * overlap]),
-#             axis=None
-#         ),
-#         alt.Fill(
-#             'mean_temp:Q',
-#             legend=None,
-#             scale=alt.Scale(domain=[30, 5], scheme='redyellowblue')
-#         )
-#     ).facet(
-#         row=alt.Row(
-#             'Month:T',
-#             title=None,
-#             header=alt.Header(labelAngle=0, labelAlign='right', format='%B')
-#         )
-#     ).properties(
-#         title='Seattle Weather',
-#         bounds='flush'
-#     ).configure_facet(spacing=0).configure_view(stroke=None).configure_title(anchor='end')
+    chart = alt.Chart(source, height=step).transform_timeunit(
+        Month='month(date)'
+    ).transform_joinaggregate(
+        mean_temp='mean(temp_max)', groupby=['Month']
+    ).transform_bin(
+        ['bin_max', 'bin_min'], 'temp_max'
+    ).transform_aggregate(
+        value='count()', groupby=['Month', 'mean_temp', 'bin_min', 'bin_max']
+    ).transform_impute(
+        impute='value', groupby=['Month', 'mean_temp'], key='bin_min', value=0
+    ).mark_area(
+        interpolate='monotone',
+        fillOpacity=0.8,
+        stroke='lightgray',
+        strokeWidth=0.5
+    ).encode(
+        alt.X('bin_min:Q', bin='binned', title='Maximum Daily Temperature (C)'),
+        alt.Y(
+            'value:Q',
+            scale=alt.Scale(range=[step, -step * overlap]),
+            axis=None
+        ),
+        alt.Fill(
+            'mean_temp:Q',
+            legend=None,
+            scale=alt.Scale(domain=[30, 5], scheme='redyellowblue')
+        )
+    ).facet(
+        row=alt.Row(
+            'Month:T',
+            title=None,
+            header=alt.Header(labelAngle=0, labelAlign='right', format='%B')
+        )
+    ).properties(
+        title='Seattle Weather',
+        bounds='flush'
+    ).configure_facet(spacing=0).configure_view(stroke=None).configure_title(anchor='end')
 
-#     st.altair_chart(chart, theme=None, use_container_width=use_container_width)
+    st.altair_chart(chart, theme=None, use_container_width=use_container_width)
 
 # Initialize state
 if "active_view" not in st.session_state:
