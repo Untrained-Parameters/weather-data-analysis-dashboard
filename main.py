7848c12a3163fc91a81a02abc1c5290dde943f87
dhvanil <dddesai@hawaii.edu>
2025-04-06 02:19:29 -1000

    kai

--- main.py
+++ main.py
@@ -674,7 +674,7 @@ with chat_col:
     </style>
     """, unsafe_allow_html=True)
 
-    with st.expander("🌐 Kai Climate Helper", expanded=True):
+    with st.expander("🌐 Kai Climate Helper", expanded=False):
         chat_history = st.session_state.setdefault("chat_history", [])
 
         if not chat_history:
